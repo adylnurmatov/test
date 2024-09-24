4a92cb6d36d7ceeaaa297ae2c3fb4d5fbffabd28
adyl <nurmatovadyl8@gmail.com>
2024-09-24 23:18:59 +0600

    ff

---
+++ Main.java
@@ -0,0 +1,24 @@
+
+
+public class Main {
+    public static void main(String[] args){
+        // JFrame frame = new JFrame();
+        // frame.setTitle("Adyl");
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // ImageIcon imageIcon = new ImageIcon("Scarecrow.png"); // load the image to a imageIcon
+        // Image image = imageIcon.getImage(); // transform it 
+        // Image newimg = image.getScaledInstance(120, 120,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+        // imageIcon = new ImageIcon(newimg);
+        // JLabel label = new JLabel();
+        // label.setText("fjfjfjf");
+        // label.setIcon(imageIcon);
+        // label.setHorizontalTextPosition(JLabel.CENTER);
+        // label.setVerticalTextPosition(JLabel.TOP);
+        // frame.setSize(500, 500);
+        // frame.setVisible(true);
+        // label.setForeground(Color.GREEN);
+        // label.setFont(new Font("MV Boli", Font.PLAIN, 20));
+        // frame.add(label);
+       new Button();
+    }
+}
